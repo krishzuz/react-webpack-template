1da8907b838c435c88e5c1eb0aaf1c5acf49a4f4
krishzuz <krishkl2001@gmail.com>
2024-09-08 14:58:40 +0530

    pushing basic link tag

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      dashboard
+      <Link to="/about"> about </Link>
+    </div>
+  );
 }
